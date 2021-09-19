9fd5088ef37284ba95ebf7ece41517f9be244951
Dongha_lee <86233889+Dongha929@users.noreply.github.com>
2021-09-19 20:18:03 +0900

    Use DFS and change data structure to 2dim list
    매우 깔끔하게 이해를 잘해서 노트로 정리도 해가며 짰는데
    1시간정도 걸린다
    노트에 정리를 잘하면 빨리 짤 수 있다
    하지만 9%에서 시간초과가 걸렸다

--- boj/#15684.py
+++ boj/#15684.py
@@ -1,56 +1,52 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
-answer = 1e9
 N, M, H = map(int, input().split())
-lines = [[] for _ in range(H+1)]
+board = [[False] * (N+1) for _ in range(H+1)]
 for _ in range(M):
   h, n = map(int, input().split())
-  lines[h].append(n)
-std = [x for x in range(N+1)]
+  board[h][n] = True
+ans = 1e9
 
-def cal_result(lines):
-  result = std[:]
-  for i in range(1, H+1):
-    for x in lines[i]:
-      result[x], result[x+1] = result[x+1], result[x]
-  return (result == std)
+def cal_result():
+  for n in range(1, N+1):
+    tmp = n
+    for i in range(1, H+1):
+      if board[i][tmp]:
+        tmp += 1
+      else:
+        if tmp != 1 and board[i][tmp-1]:
+          tmp -= 1
+    if tmp != n:
+      return False
+  return True
 
-def cal_case(lines):
-  result = []
-  for i in range(1, H+1):
-    for j in range(1, N):
-      if j not in lines[i] and j + 1 not in lines[i] and j - 1 not in lines[i]:
-        result.append((i, j))
-  return result
+def cal_cases():
+  res = []
+  for h in range(1, H+1):
+    for n in range(1, N):
+      if not board[h][n] and not board[h][n+1] and not board[h][n-1]:
+        res.append([h, n])
+  return res
 
-# visited가 너무 크다
-visited = set()
-def BFS(q):
-  global answer
-  while q:
-    case = q.popleft()
-    if len(case) == 4 or len(case) >= answer:
-      break
-    nlines = [x[:] for x in lines]
-    for ele in case:
-      h, n = ele
-      nlines[h].append(n)
-    if cal_result(nlines):
-      answer = min(answer, len(case))
+def DFS(d):
+  global ans
+  if ans <= d or d == 3:
+    return
+  res = cal_cases()
+  for case in res:
+    h, n = case
+    board[h][n] = True
+    if cal_result():
+      ans = d + 1
+      board[h][n] = False
+      return
     else:
-      for next in cal_case(nlines):
-        ncase = case.copy()
-        ncase.add(next)
-        if ncase in visited:
-          continue
-        else:
-          visited.add(frozenset(ncase))
-          q.append(ncase)
+      DFS(d+1)
+      board[h][n] = False
 
-q = deque([])
-a = set()
-q.append(a)
-BFS(q)
-print(answer if answer != 1e9 else -1)
+if cal_result():
+  ans = 0
+else:
+  DFS(0)
+print(ans if ans != 1e9 else -1)
